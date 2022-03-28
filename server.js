6cff214e54ad7967a39cde40da3740e71ab01f6e
Francis Gauthier <fgauthier@crc-lennox.qc.ca>
2022-03-28 13:16:08 -0400

    Fix missing import: user service

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const { connectCallback } = require('./database');
-const { createUserSimple, createUserBetter, signIn } = require('./user.service');
 const bodyParser = require('body-parser');
 const boatRouter = require('./routers/boat.router');
 const app = express();
